0699f4d3e9be17d63edda97226f580cbcdbb5c67
Stephen Tilley <stephentilley@gmail.com>
2018-02-05 16:55:17 +0000

    Using params

--- app.rb
+++ app.rb
@@ -22,7 +22,13 @@ get '/french' do
   "bonjour!"
 end
 
-get '/cat' do
-  @random_name = ["Amigo", "Oscar", "Viking"].sample
+get '/random-cat' do
+  @name = ["Amigo", "Oscar", "Viking"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
